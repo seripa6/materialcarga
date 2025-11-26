c4c68783cf57e703e7da035d7d7b42d05bdb72f8
seripa6 <infraestrutura.seripa6@gmail.com>
2025-11-26 09:52:56 -0300

    Delete js directory

--- js/teste.js
+++
@@ -1,9 +0,0 @@
-var willWatchMovie = function(ticketCost, isActionFlick) { 
-    if (isActionFlick === true && ticketCost > 5) {
-        return "no";
-    } else if (ticketCost < 15) {
-        return "yes";
-    } else {
-       return "maybe";
-    }
-};
